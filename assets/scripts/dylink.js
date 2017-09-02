06c420f2f6ad7520734bf0481f34a7ad182e417a
Dior Phillips <diorphillips@Ds-MacBook-Pro.local>
2017-09-02 12:50:51 -0400

    suicide shit
    cool lol

--- assets/scripts/dylink.js
+++ assets/scripts/dylink.js
@@ -1,4 +1,11 @@
-dirs = ["HOME", "TERMS", "MEDIA", "MUSIC", "SHOP"]; // List of each directory
+dirs = [
+"Everything will be okay.",
+"Life is boring without a few challenges.",
+"Regardless of how many doors are in your way, keep moving forward.",
+"Embrace every piece of hate you receive, use each person who has ever talked down to you as a medal and count them as a blessing that you received what you did from them before you are 'up,' prove every person who told you otherwise wrong, don't ever let hate from others interfere with your success. It's my advantage. Hate me."];
+
+
+// List of each directory
 page = document.getElementById('dynamic_link').innerHTML; // Get the inner HTML element of the current page
 value = dirs.indexOf(page);
 i = value;
